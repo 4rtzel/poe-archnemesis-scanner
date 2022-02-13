6284bd6449586adfaa85fca242688923e06a9f5c
Art Zel <4rtzel@gmail.com>
2022-02-13 14:15:38 +0300

    Display message box when the game is not running. Display message box when we weren't able to detect game resolution. Fallback to the screen resolution in that case

--- poe_arch_scanner.py
+++ poe_arch_scanner.py
@@ -6,6 +6,7 @@ import win32gui
 from win32clipboard import *
 
 import tkinter as tk
+from tkinter import messagebox
 from typing import Callable, Any, Tuple, List, Dict
 
 import cv2
@@ -278,6 +279,7 @@ class UIOverlay:
         self._root.overrideredirect(True)
         self._root.geometry(f'+{info.x + 5}+{info.y + info.title_bar_height + 5}')
         self._root.wm_attributes('-topmost', True)
+        self._root.deiconify()
 
     @staticmethod
     def create_toplevel_window(bg=''):
@@ -623,9 +625,20 @@ class Settings:
     def should_copy_recipe_to_clipboard(self) -> bool:
         return self._copy_recipe_to_clipboard
 
+def show_warning(text: str) -> None:
+    messagebox.showwarning('poe-archnemesis-scanner', text)
+
+def show_error_and_die(text: str) -> None:
+    # Dealing with inconveniences as Perl would
+    messagebox.showerror('poe-archnemesis-scanner', text)
+    sys.exit()
+
 def get_poe_window_info() -> PoeWindowInfo:
     info = PoeWindowInfo()
     hwnd = win32gui.FindWindow(None, 'Path of Exile')
+    if hwnd == 0:
+        show_error_and_die('Path of Exile is not running.')
+
     x0, y0, x1, y1 = win32gui.GetWindowRect(hwnd)
     info.x = x0
     info.y = y0
@@ -634,6 +647,14 @@ def get_poe_window_info() -> PoeWindowInfo:
     x0, y0, x1, y1 = win32gui.GetClientRect(hwnd)
     info.client_width = x1 - x0
     info.client_height = y1 - y0
+
+    if info.client_width == 0 or info.client_height == 0:
+        show_warning("Unable to detect Path of Exile resolution. Make sure it isn't running in the Fullscreen mode.\n\nThe tool will use your screen resolution for calculations instead.")
+        screen = ImageGrab.grab()
+        info.x = 0
+        info.y = 0
+        info.width, info.height = screen.size
+        info.client_width, info.client_height = screen.size
     info.title_bar_height = info.height - info.client_height
     return info
 
@@ -654,6 +675,7 @@ def calculate_default_scale(info: PoeWindowInfo) -> float:
 
 # Create root as early as possible to initialize some modules (e.g. ImageTk)
 root = tk.Tk()
+root.withdraw()
 
 info = get_poe_window_info()
 
