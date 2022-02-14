e642b723075361dd9abb49091b5396420cd8f3b1
William Metcalf <williamdmetcalf@gmail.com>
2022-02-13 20:50:43 -0500

    added RecipeShopper class
    computes missing items from a list of desired items and the inventory scan result

---
+++ src/RecipeShopper.py
@@ -0,0 +1,38 @@
+from copy import deepcopy
+from typing import Dict, List, Tuple
+from ArchnemesisItemsMap import ArchnemesisItemsMap
+
+class RecipeShopper:
+  def __init__(self, item_map: ArchnemesisItemsMap):
+    self._item_map = item_map
+
+  def get_missing_items(self, desired_items: List[str], current_inventory: Dict[str, List[Tuple[int, int]]]):
+    missing_items = list()
+    # clone the inventory, because we're going to take things out of it as owned
+    # items are identified in case dupes are needed
+    working_inventory = deepcopy(current_inventory)
+
+    # identify which items are not already owned,
+    # and remove owned items from the working inventory
+    for item in desired_items:
+      if not is_item_owned(working_inventory, item):
+        missing_items.append(item)
+      else:
+        remove_item_from_inventory(working_inventory, item)
+
+    # recursively call this function passing the ingredients for missing items
+    complex_missing_items = list(item for item in missing_items if len(self._item_map.get_subtree_for(item).components) > 0)
+    if len(complex_missing_items) > 0:
+      complex_item_ingredients = list()
+      for item in complex_missing_items:
+        complex_item_ingredients.extend(self._item_map._get_item_components(item))
+      nested_missing_items = self.get_missing_items(complex_item_ingredients, working_inventory)
+      missing_items.extend(nested_missing_items)
+
+    return missing_items
+
+def is_item_owned(inventory: Dict[str, List[Tuple[int, int]]], item: str):
+  return item in inventory.keys() and len(inventory[item]) > 0
+
+def remove_item_from_inventory(inventory:  Dict[str, List[Tuple[int, int]]], item: str):
+  return inventory[item].pop()
---
+++ src/RecipeShopper.test.py
@@ -0,0 +1,66 @@
+import unittest
+import tkinter as tk
+
+from ArchnemesisItemsMap import ArchnemesisItemsMap
+from RecipeShopper import RecipeShopper
+
+class TestStringMethods(unittest.TestCase):
+    def test_owned_items(self):
+      item_map = ArchnemesisItemsMap(1)
+      self._shopper = RecipeShopper(item_map)
+      desired_items = ["Effigy", "Corrupter"]
+      inventory = {
+        "Effigy": [ [1, 2] ], 
+        "Corrupter": [ [1, 2] ]
+        }
+
+      missing_items = self._shopper.get_missing_items(desired_items, inventory)
+      self.assertListEqual(missing_items, [])
+
+    def test_missing_simple_items(self):
+      item_map = ArchnemesisItemsMap(1)
+      self._shopper = RecipeShopper(item_map)
+      desired_items = ["Effigy", "Berserker"]
+      inventory = {
+        "Effigy": [ [1, 2] ], 
+        }
+
+      missing_items = self._shopper.get_missing_items(desired_items, inventory)
+      self.assertListEqual(missing_items, ["Berserker"])
+
+    def test_missing_complex_items(self):
+      item_map = ArchnemesisItemsMap(1)
+      self._shopper = RecipeShopper(item_map)
+      desired_items = ["Effigy"]
+      inventory = {}
+
+      missing_items = self._shopper.get_missing_items(desired_items, inventory)
+      self.assertListEqual(missing_items, ['Effigy', 'Hexer', 'Malediction', 'Corrupter', 'Chaosweaver', 'Echoist', 'Bloodletter', 'Chaosweaver'])
+
+    def test_missing_complex_items_with_partial_inventory(self):
+      item_map = ArchnemesisItemsMap(1)
+      self._shopper = RecipeShopper(item_map)
+      desired_items = ["Effigy"]
+      inventory = {
+        "Corrupter": [ [1, 2] ],
+        "Echoist": [ [1, 2] ]
+      }
+
+      missing_items = self._shopper.get_missing_items(desired_items, inventory)
+      self.assertListEqual(missing_items, ['Effigy', 'Hexer', 'Malediction', 'Chaosweaver'])
+
+    def test_missing_requiring_duplicates_with_partial_inventory(self):
+      item_map = ArchnemesisItemsMap(1)
+      self._shopper = RecipeShopper(item_map)
+      desired_items = ["Assassin", "Assassin"]
+      inventory = {
+        "Deadeye": [ [1, 2], [1, 2] ],
+        "Vampiric": [ [1, 2] ]
+      }
+
+      missing_items = self._shopper.get_missing_items(desired_items, inventory)
+      self.assertListEqual(missing_items, ['Assassin', 'Assassin', 'Vampiric'])
+
+if __name__ == '__main__':
+  root = tk.Tk()
+  unittest.main()
